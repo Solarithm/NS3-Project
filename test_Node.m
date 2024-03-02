5d4bce0096c8bf2efe1b4dadbd5d375792ea4c4b
TuanTran <tuandinhhd08112002@gmail.com>
2024-03-03 01:20:54 +0700

    add_Prim_ and energy

--- test_Node.m
+++ test_Node.m
@@ -1,16 +1,25 @@
+clc;
 clear;
-n=15; 
-R = 15; % Radius in range of sensor Nodes
 
-x = [-10,-5,-4,7,10,12,17,22,25,28,31,35,37,42,46];
-y = [20,27,16,21,35,12,17,28,24,25,38,20,32,26,30];
+
+grid on
+figure(1) % Hold figure 1
+hold on
+box on
+xlabel (' Length (m)') % X-label of the output plot
+ylabel (' Width (m)') % Y-label of the output plot
+title (' Simulator') % Title of the plot
+
+
+n = 15; 
+R = 11; % Radius in range of sensor Nodes
+
+x = [-5,3,4,7,12,12,17,22,27,33,31,35,37,46,51];
+y = [20,23,18,21,27,12,17,28,21,25,26,24,26,26,30];
 
 matrix = zeros(15,15);
 
 adj_matrix = zeros(15,15);
-
-x1 = zeros(1,15);
-y1 = zeros(1,15);
 
 node(1,15) = Nodes();
 s = [];
@@ -47,74 +56,29 @@ for i = 1 : n
    end
        count = count + 1;
 end
-G = digraph(s,t);
-p = G;
-%p.LineWidth = 1.5;
-%p.MarkerEdgeColor = k;
-% p.MarkerSize = 10;
-% p.MarkerFaceColor = [1 1 0];
-
-figure = plot(p,'XData',x,'YData',y)
-%     'LineWidth',1.5,... % Size of the line
-%     'MarkerEdgeColor','k',... % The color of the outer surface of the node. Currently it is set to black color. "k" stand for black.
-%     'MarkerFaceColor', [1 1 0],... % The color of the inside of the node. Currently it is set to yellow color. " [1 1 0]" is a code of yellow color
-%     'MarkerSize',10
-
-figure.NodeColor = 'r';
-
-
-path = shortestpath(p,1,15);
-
-% Packet transmission
-
-
-path2 = shortestpath(p,1,15);
-
-s_plot = [];
-t_plot = [];
-x_plot =[];
-y_plot = [];
-
-packet = 0;
-num = [];
-str = {};
-
-% while(packet < 3)
-% while(length(path) > 1)
-%     for i = 1:15
-%         num(i,packet+1) = node(i).E_intial;
-%     if(i == path(1))
-%         num(i,packet+1) = node(i).E_intial;
-%         node(i).update_energy_Tx;
-%         array_index = find(s == path(1));
-%        for k = 1:length(array_index)
-%         m = t(array_index(k));
-%         node(m).update_energy_Rx;
-%         num(m,packet+1) = node(m).E_intial;
-%        end
-% %         pause(1);
-%         path = path(2:end);
-%         break;
-%     end  
-%      
-%     end 
-% end
-%     packet = packet + 1;
-%     path = path2;
-% end
 
 %check_neighbor
+neighbor = [];
+ID = [];
 for i = 1:15
-    array_index = find(s == i);
-    neighbor = t(array_index);
-    node(i).neighbor = neighbor;
+    %Node Id 
+    ID = [ID,i];
+    node(i).ID = i;
+    node(i).d0 = sqrt(node(i).Efs/node(i).Emp);
+    for j = 1:15
+    %Node neighbor;
+     if(adj_matrix(i,j) == 1)
+       neighbor = [neighbor, j];
+       node(i).neighbor = neighbor;
+        end
+    end
+    neighbor = [];
 end
 
 %distance
 d = [];
 for i = 1 : 15
-         array_index = find(s == i);
-         neighbor = t(array_index);
+         neighbor = node(i).neighbor;
          for j = 1 : 15
              if (ismember(j,neighbor))
                  d = [d, matrix(i,j)];
@@ -124,26 +88,42 @@ for i = 1 : 15
          d = [];
 end
 
-
-%     for i = 1 : 15   
-%      x1(i) = x(i) + 1;
-%      y1(i) = y(i) + 1;
-%     
-%      str{i,1} = num(i,packet)
-%     end
-    
-text(x1,y1,str);
+G = digraph(s,t);
+p = G;
+figure = plot(p,'XData',x,'YData',y)
+figure.NodeColor = 'r';
 
 
-grid on
-figure(1) % Hold figure 1
-hold on
-box on
-xlabel (' Length (m)') % X-label of the output plot
-ylabel (' Width (m)') % Y-label of the output plot
-% zlabel (' Height (m)') % Z-label of the output plot
-title (' Simulator') % Title of the plot
-grid on % Activate the grid in background of the plot
-% Hold figure 1
-%Note: To change the color, just write "r" for red, "g" for green, "b" for
-%blue, "m" for magenta, "c" for cyan and so on
+
+%% Add energy 
+Energy(x,y,node);
+
+%%
+
+
+% G1 = graph(s,t);
+% T1 = minspantree(G1);
+% figure1 =  plot(T1,'XData',x,'YData',y)
+% figure1.LineStyle = '-';
+% figure1.LineWidth = 2;
+% figure1.NodeColor = 'r';
+        
+      
+%% Prim_local
+
+MST_local = Prim(8,13,node);
+
+s8_13 = MST_local(:,1);
+t8_13 = MST_local(:,2);
+for i = 1 : size(MST_local,1)
+        x1 = x(MST_local(i, 1));
+        y1 = y(MST_local(i, 1));
+        x2 = x(MST_local(i, 2));
+        y2 = y(MST_local(i, 2));
+        h = line([x1, x2], [y1, y2],'LineStyle','-','LineWidth',2,'Color', 'y');
+end
+
+               
+
+
+
