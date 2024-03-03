c43e615e40aef2ef3a1cb899895c824c67f0bb10
Solarithm <93081062+Solarithm@users.noreply.github.com>
2024-03-03 16:40:29 +0700

    Update Node.m

--- Node.m
+++ Node.m
@@ -1,44 +1,33 @@
-classdef Nodes
-    properties
-        x
-        y
-        neighbor
-        distance
-        % Energy 
-        energyInitial % Năng lượng ban đầu
-        energyConsumed % Năng lượng tiêu hao
-        energyRemaining % Năng lượng còn lại
-
+    classdef Node
+        properties
+            x
+            y
+            neighbor
+            distance
+            eI
+            eC
+            eR
+        end
+        methods
+            % Node contructor
+            function obj = NodeUnEnergy(x, y)
+                obj.x = x;
+                obj.y = y;
+                obj.neighbor = [];
+                obj.distance = 0;
+            end
+            function obj = Node(x, y)
+                obj.x = x;
+                obj.y = y;
+                obj.neighbor = [];
+                obj.distance = 0;
+                 % Energy constructor
+                obj.eI = 100;
+                obj.eC = 0; 
+                obj.eR = 100;
+            end 
+        end
     end
-    methods
-        % Node contructor
-        function obj = Nodes(x, y)
-            obj.x = x;
-            obj.y = y;
-            obj.neighbor = [];
-            obj.distance = 0;
-        end
-        function nodes = deleteNode(nodes, index)
-            nodes(index) = [];
-        end
-        function obj = NodeswEnergy(x, y, energyInitial)
-            obj.x = x;
-            obj.y = y;
-            obj.neighbor = [];
-            obj.distance = 0;
-             % Energy constructor
-            obj.energyInitial = energyInitial; % Gán năng lượng ban đầu
-            obj.energyConsumed = 0; % Khởi tạo năng lượng tiêu hao là 0
-            obj.energyRemaining = energyInitial; % Gán năng lượng còn lại bằng năng lượng ban đầu
-        end 
-        % Energy consumption
-        function consumeEnergy(obj, energy)
-            obj.energyConsumed = obj.energyConsumed + energy;
-            obj.energyRemaining = obj.energyInitial - obj.energyConsumed;
-        end
-
-    end
-end
 
 
 
