92deb298a91c6568790d99bff5543771ffe7d35e
Nhat <nhathoang290502@gmail.com>
2024-03-04 17:49:55 +0700

    Animation

--- Node.m
+++ Node.m
@@ -1,33 +1,38 @@
-    classdef Node
-        properties
-            x
-            y
-            neighbor
-            distance
-            eI
-            eC
-            eR
-        end
-        methods
-            % Node contructor
-            function obj = NodeUnEnergy(x, y)
-                obj.x = x;
-                obj.y = y;
-                obj.neighbor = [];
-                obj.distance = 0;
+classdef Node < handle
+    
+    properties
+        E_initial = 2;
+        E_tx;
+        E_rx;
+        Packet_Size = 500; %bytes
+        Elec = 50 * 0.000000001; % J/bit
+        Eamp = 100 * 0.000000000001; %J
+        Efs = 10 * 0.000000000001; % J/bit/m^2
+        Emp = 0.0013 * 0.000000000001; %J/bit/m^4
+        d; %distance
+        d0; %thresh hold
+        B = 500 * 1024; %bit 
+        neighbor ;
+        ID;
+        parent;
+        child;
+        link; %connection to node neighbor
+    end
+  
+    
+    methods
+        function change_energy_Tx(obj)
+            for i = length(obj.neighbor)
+                if(obj.d(i) < obj.d0)
+                     obj.E_tx = (obj.B * obj.Elec) + (obj.B * obj.Efs * (obj.d(i)^2));
+                else
+                     obj.E_tx = (obj.B * obj.Elec) + (obj.B * obj.Efs * (obj.d(i)^4));
+                end              
             end
-            function obj = Node(x, y)
-                obj.x = x;
-                obj.y = y;
-                obj.neighbor = [];
-                obj.distance = 0;
-                 % Energy constructor
-                obj.eI = 100;
-                obj.eC = 0; 
-                obj.eR = 100;
-            end 
+        end
+    
+        function change_energy_Rx(obj)
+                obj.E_rx = obj.B*obj.Elec;
         end
     end
-
-
-
+end
---
+++ T_test_Node.m
@@ -0,0 +1,144 @@
+%%
+clc;
+clear;
+grid on
+figure() % Hold figure 1
+hold on
+box on
+xlabel (' Length (m)') % X-label of the output plot
+ylabel (' Width (m)') % Y-label of the output plot
+title (' Simulator') % Title of the plot
+
+%% Making Network
+n = 15; 
+R = 11; % Radius in range of sensor Nodes
+
+x = [-5,3,4,7,12,12,17,22,27,33,31,35,37,46,51];
+y = [20,23,18,21,27,12,17,28,21,25,26,24,26,26,30];
+    
+matrix = zeros(n,n);
+
+adj_matrix = zeros(n,n);
+
+nodes(1,n) = Node();
+s = [];
+t = [];
+
+clf;
+hold on;
+plot (x,y,'mo',... % Plot all the nodes in 2 dimension
+    'LineWidth',1.5,... % Size of the line
+    'MarkerEdgeColor','k',... % The color of the outer surface of the node. Currently it is set to black color. "k" stand for black.
+    'MarkerFaceColor', [1 1 0],... % The color of the inside of the node. Currently it is set to yellow color. " [1 1 0]" is a code of yellow color
+    'MarkerSize',10)
+
+for i = 1 : n
+    for j = 1 : n
+        distance = sqrt((x(i) - x(j))^2 + (y(i) - y(j))^2);
+        matrix(i,j) = distance;
+         if (i == j)
+            adj_matrix(i,j) = 0;
+         elseif (i~=j && distance < R) 
+            adj_matrix(i,j) = 1;
+         else
+             adj_matrix(i,j) = inf;
+         end       
+     end
+end
+
+count =2;
+for i = 1 : n
+   for j = count : n
+    if(adj_matrix(i,j) == 1)
+       s = [s, i];
+       t = [t, j];
+    end
+   end
+       count = count + 1;
+end
+
+%% Simulation
+timeStart = 1;
+timeEnd = 100;
+for timeStep = timeStart : timeEnd
+    %% Check_neighbor
+    neighbor = [];
+    ID = [];
+    for i = 1:n
+        %Node Id 
+        ID = [ID,i];
+        nodes(i).ID = i;
+        nodes(i).d0 = sqrt(nodes(i).Efs/nodes(i).Emp);
+        for j = 1:n
+        %Node neighbor;
+         if(adj_matrix(i,j) == 1)
+           neighbor = [neighbor, j];
+           nodes(i).neighbor = neighbor;
+            end
+        end
+        neighbor = [];
+    end
+
+    %% Edge Weights
+    distances = [];
+    linkQuality = [];
+    weight = 0;
+    for i = 1 : n
+        for j = (i+1) : n 
+            if matrix(i,j) < R
+                weight = weight + 1;
+                distances(weight) = matrix(i,j);
+                linkQuality(weight) = nodes(i).E_initial*exp(-distances(weight));
+                nodes(i).link = linkQuality(weight);
+            end
+        end
+    end
+    %distance
+    d = [];
+    link = [];
+    for i = 1 : n
+         neighbor = nodes(i).neighbor;
+         for j = 1 : n
+             if (ismember(j,neighbor))
+                 d = [d, matrix(i,j)];
+                 nodes(i).d = d;
+                 link = [link, matrix(i,j)];
+                 nodes(i).link = nodes(i).E_initial*exp(-nodes(i).d);
+             end
+         end
+         d = [];
+         link = [];
+    end
+
+    %% Graph    
+    G = digraph(s,t);
+    figure = plot(G,'XData',x,'YData',y, 'EdgeLabel', linkQuality)
+    figure.NodeColor = 'r';
+    
+    %% Add energy 
+    Energy(x,y,nodes);
+
+    %% Prim_local
+    MST_lobal = Prim(1, 13, nodes);
+    for i = 1 : size(MST_lobal,1)
+            x1 = x(MST_lobal(i, 1));
+            y1 = y(MST_lobal(i, 1));
+            x2 = x(MST_lobal(i, 2));
+            y2 = y(MST_lobal(i, 2));
+            h = line([x1, x2], [y1, y2]);
+            h.LineStyle = '-';
+            h.LineWidth = 2;
+            r = rand(1, 1); 
+            g = rand(1, 1); 
+            b = rand(1,1);
+            h.Color = [r g b];
+            drawnow;
+    end
+    pause(0.1);
+    hold off;
+end
+
+
+ 
+
+
