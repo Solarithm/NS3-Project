e3f0f33cc8869584ab3b271c9686bf533699de19
TuanTran <tuandinhhd08112002@gmail.com>
2024-03-04 10:54:52 +0700

    Update Energy and Prim

---
+++ Nodes.m
@@ -0,0 +1,38 @@
+classdef Nodes < handle
+    
+    properties
+        E_intial = 2;
+        E_tx;
+        E_rx;
+        Packet_Size = 500; %bytes
+        Elec = 50 * 0.000000001; % J/bit
+        Eamp = 100 * 0.000000000001; %J
+        Efs = 10 * 0.000000000001; % J/bit/m^2
+        Emp = 0.0013 * 0.000000000001; %J/bit/m^4
+        d; %distance
+        d0; %thresh hold
+        B = 500 * 1024; %bit 
+        neighbor ;
+        ID;
+        parent;
+        child;
+        
+    end
+  
+    
+    methods
+        function change_energy_Tx(obj)
+            for i = length(obj.neighbor)
+                if(obj.d(i) < obj.d0)
+                     obj.E_tx = (obj.B * obj.Elec) + (obj.B * obj.Efs * (obj.d(i)^2));
+                else
+                     obj.E_tx = (obj.B * obj.Elec) + (obj.B * obj.Efs * (obj.d(i)^4));
+                end              
+            end
+        end
+    
+        function change_energy_Rx(obj)
+                obj.E_rx = obj.B*obj.Elec;
+        end
+    end
+end
--- test_Node.m
+++ test_Node.m
@@ -1,18 +1,27 @@
+
+%%
+clc;
 clear;
-n=15; 
-R = 15; % Radius in range of sensor Nodes
+grid on
+figure(1) % Hold figure 1
+hold on
+box on
+xlabel (' Length (m)') % X-label of the output plot
+ylabel (' Width (m)') % Y-label of the output plot
+title (' Simulator') % Title of the plot
 
-x = [-10,-5,-4,7,10,12,17,22,25,28,31,35,37,42,46];
-y = [20,27,16,21,35,12,17,28,24,25,38,20,32,26,30];
+%% Making Network
+n = 15; 
+R = 11; % Radius in range of sensor Nodes
 
-matrix = zeros(15,15);
+x = [-5,3,4,7,12,12,17,22,27,33,31,35,37,46,51];
+y = [20,23,18,21,27,12,17,28,21,25,26,24,26,26,30];
 
-adj_matrix = zeros(15,15);
+matrix = zeros(n,n);
 
-x1 = zeros(1,15);
-y1 = zeros(1,15);
+adj_matrix = zeros(n,n);
 
-node(1,15) = Nodes();
+node(1,n) = Nodes();
 s = [];
 t = [];
 
@@ -47,75 +56,30 @@ for i = 1 : n
    end
        count = count + 1;
 end
-G = digraph(s,t);
-p = G;
-%p.LineWidth = 1.5;
-%p.MarkerEdgeColor = k;
-% p.MarkerSize = 10;
-% p.MarkerFaceColor = [1 1 0];
-
-figure = plot(p,'XData',x,'YData',y)
-%     'LineWidth',1.5,... % Size of the line
-%     'MarkerEdgeColor','k',... % The color of the outer surface of the node. Currently it is set to black color. "k" stand for black.
-%     'MarkerFaceColor', [1 1 0],... % The color of the inside of the node. Currently it is set to yellow color. " [1 1 0]" is a code of yellow color
-%     'MarkerSize',10
-
-figure.NodeColor = 'r';
-
-
-path = shortestpath(p,1,15);
-
-% Packet transmission
-
-
-path2 = shortestpath(p,1,15);
-
-s_plot = [];
-t_plot = [];
-x_plot =[];
-y_plot = [];
-
-packet = 0;
-num = [];
-str = {};
-
-% while(packet < 3)
-% while(length(path) > 1)
-%     for i = 1:15
-%         num(i,packet+1) = node(i).E_intial;
-%     if(i == path(1))
-%         num(i,packet+1) = node(i).E_intial;
-%         node(i).update_energy_Tx;
-%         array_index = find(s == path(1));
-%        for k = 1:length(array_index)
-%         m = t(array_index(k));
-%         node(m).update_energy_Rx;
-%         num(m,packet+1) = node(m).E_intial;
-%        end
-% %         pause(1);
-%         path = path(2:end);
-%         break;
-%     end  
-%      
-%     end 
-% end
-%     packet = packet + 1;
-%     path = path2;
-% end
 
 %check_neighbor
-for i = 1:15
-    array_index = find(s == i);
-    neighbor = t(array_index);
-    node(i).neighbor = neighbor;
+neighbor = [];
+ID = [];
+for i = 1:n
+    %Node Id 
+    ID = [ID,i];
+    node(i).ID = i;
+    node(i).d0 = sqrt(node(i).Efs/node(i).Emp);
+    for j = 1:n
+    %Node neighbor;
+     if(adj_matrix(i,j) == 1)
+       neighbor = [neighbor, j];
+       node(i).neighbor = neighbor;
+        end
+    end
+    neighbor = [];
 end
 
 %distance
 d = [];
-for i = 1 : 15
-         array_index = find(s == i);
-         neighbor = t(array_index);
-         for j = 1 : 15
+for i = 1 : n
+         neighbor = node(i).neighbor;
+         for j = 1 : n
              if (ismember(j,neighbor))
                  d = [d, matrix(i,j)];
                  node(i).d = d;
@@ -124,26 +88,106 @@ for i = 1 : 15
          d = [];
 end
 
+G = digraph(s,t);
+p = G;
+figure = plot(p,'XData',x,'YData',y)
+figure.NodeColor = 'r';
 
-%     for i = 1 : 15   
-%      x1(i) = x(i) + 1;
-%      y1(i) = y(i) + 1;
-%     
-%      str{i,1} = num(i,packet)
-%     end
+
+
+%% Add energy 
+Energy(x,y,node);
+
+%% Prim - Global
+
+% G1 = graph(s,t);
+% T1 = minspantree(G1);
+% figure1 =  plot(T1,'XData',x,'YData',y)
+% figure1.LineStyle = '-';
+% figure1.LineWidth = 2;
+% figure1.NodeColor = 'r';
+        
+      
+%% Prim_local
+MST_global = Prim(1,13,node);
+for i = 1 : size(MST_global,1)
+        x1 = x(MST_global(i, 1));
+        y1 = y(MST_global(i, 1));
+        x2 = x(MST_global(i, 2));
+        y2 = y(MST_global(i, 2));
+        h = line([x1, x2], [y1, y2]);
+        h.LineStyle = '-';
+        h.LineWidth = 2;
+        h.Color = [0.9290 0.6940 0.1250];
+end
+
+% MST_local2 = Prim(4,9,node);
+% for i = 1 : size(MST_local2,1)
+%         x1 = x(MST_local2(i, 1));
+%         y1 = y(MST_local2(i, 1));
+%         x2 = x(MST_local2(i, 2));
+%         y2 = y(MST_local2(i, 2));
+%         h = line([x1, x2], [y1, y2]);
+%         h.LineStyle = '-';
+%         h.LineWidth = 2;
+%         h.Color = [0.9290 0.6940 0.1250];
+% end
+% 
+% MST_local1 = Prim(1,4,node);
+% for i = 1 : size(MST_local1,1)
+%         x1 = x(MST_local1(i, 1));
+%         y1 = y(MST_local1(i, 1));
+%         x2 = x(MST_local1(i, 2));
+%         y2 = y(MST_local1(i, 2));
+%         h = line([x1, x2], [y1, y2]);
+%         h.LineStyle = '-';
+%         h.LineWidth = 2;
+%         h.Color = [0.4940 0.1840 0.5560];
+% end
+% 
+% MST_local3 = Prim(9,13,node);
+% for i = 1 : size(MST_local3,1)
+%         x1 = x(MST_local3(i, 1));
+%         y1 = y(MST_local3(i, 1));
+%         x2 = x(MST_local3(i, 2));
+%         y2 = y(MST_local3(i, 2));
+%         h = line([x1, x2], [y1, y2]);
+%         h.LineStyle = '-';
+%         h.LineWidth = 2;
+%         h.Color = 'k';
+% end
+  
+%% Transmission Packet 
+%Nhan thong tin tu vung 6,11,12,8
+
+%Check parent
+parent = MST_global(:,1);
+child = MST_global(:,2);
+for i = 1: size(MST_global,1)
+    array_col = find(i == parent);
+    array_col2 = find(i == child);
+    c = child(array_col);
+    c2 = parent(array_col2);
+    node(i).child = c;
+    node(i).parent = c2;
+        if(i == 1)
+            node(i).parent = 0;
+        end
+end
+
+%Check routing_path
+%6 gui
+%gui cho parent
+packet_ID;
+while(packet_ID ~= 1)
+    %from 6 to parent,when parent = 1==> end while
+    %6 lose E_Tx,1 lose E_rx, and router lose E_total = E_tx + E_rx
     
-text(x1,y1,str);
+    %Send packet to parent, parent lose Rx, child lose Tx
+    while(node(i).parent ~= )
+    end
+end
+               
 
 
-grid on
-figure(1) % Hold figure 1
-hold on
-box on
-xlabel (' Length (m)') % X-label of the output plot
-ylabel (' Width (m)') % Y-label of the output plot
-% zlabel (' Height (m)') % Z-label of the output plot
-title (' Simulator') % Title of the plot
-grid on % Activate the grid in background of the plot
-% Hold figure 1
-%Note: To change the color, just write "r" for red, "g" for green, "b" for
-%blue, "m" for magenta, "c" for cyan and so on
+
