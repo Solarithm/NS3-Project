75dff849d746649f525e32ab85099e0311fec4a4
Solarithm <nhathoang290502@gmail.com>
2024-04-13 20:00:09 +0700

    optimize

--- DeleteNodesFromNeighbor.m
+++ DeleteNodesFromNeighbor.m
@@ -1,18 +1,17 @@
-function DeleteNodesFromNeighbor(node, nodes)
+function DeleteNodesFromNeighbor(nodes, node_id)
     for i = 1 : length(nodes)
         for j = 1 : length(nodes(i).neighbor)
-            if nodes(i).neighbor(j) == node
+            if nodes(i).neighbor(j) == node_id
                 nodes(i).neighbor(j) = [];
                 break;
             end           
         end
-        if nodes(i).child == node
+        if nodes(i).child == node_id
             nodes(i).child = [];
         end
         
-        if nodes(i).parent == node
+        if nodes(i).parent == node_id
             nodes(i).parent = [];
         end
-    end
-    
+    end 
 end
--- DisconnectedNode.m
+++
@@ -1,5 +0,0 @@
-%% Delete node
-function DisconnectedNode(nodes, node)
-    nodes(node).status = 0;
-    nodes(node).E_initial = 0;
-end
